d43a595770117f3c8cc6e389dace1ed952d9acbc
Cristiano Cunha <Contato@cristianoprogramador.com>
2017-11-24 19:54:29 -0200

    Address e ValueObjects

---
+++ BaltaStore.Domain/StoreContext/Entities/Address.cs
@@ -0,0 +1,40 @@
+using BaltaStore.Domain.StoreContext.Enums;
+
+namespace BaltaStore.Domain.StoreContext.Entities
+{
+    public class Address
+    {
+        public Address(
+            string street,
+            string number,
+            string complement,
+            string city,
+            string state,
+            string country,
+            string zipCode,
+            EAddressType addressType)
+        {
+            Street = street;
+            Number = number;
+            Complement = complement;
+            City = city;
+            State = state;
+            Country = country;
+            ZipCode = zipCode;
+            AddressType = addressType;
+        }
+        public string Street { get; private set; }
+        public string Number { get; private set; }
+        public string Complement { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string Country { get; private set; }
+        public string ZipCode { get; private set; }
+        public EAddressType AddressType { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Street}, {Number}  {City}/{State} {Country}";
+        }
+    }
+}
--- BaltaStore.Domain/StoreContext/Entities/Customer.cs
+++ BaltaStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,33 +1,32 @@
+using System.Collections.Generic;
+using BaltaStore.Domain.StoreContext.ValueObjects;
+
 namespace BaltaStore.Domain.StoreContext.Entities
 {
     public class Customer
     {
         public Customer(
-            string firstName,
-            string lastName,
-            string email,
-            string document,
-            string phone,
-            string address)
+            Name name,
+            Email email,
+            Document document,
+            string phone)
         {
-            FirstName = firstName;
-            LastName = lastName;
+            Name = name;
             Email = email;
             Document = document;
             Phone = phone;
-            Address = address;
+            Addresses = new List<Address>();
         }
 
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public string Document { get; private set; }
-        public string Email { get; private set; }
+        public Name Name { get; set; }
+        public Document Document { get; private set; }
+        public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public string Address { get; private set; }
+        public IReadOnlyCollection<Address> Addresses { get; private set; }
 
         public override string ToString()
         {
-            return $"{FirstName} {LastName}";
+            return Name.ToString();
         }
     }
 }
